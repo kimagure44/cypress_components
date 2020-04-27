6bb8e2cedd08ab4766eabd5c730659874064eae9
kimagure44 <ivan.lara.gomez.1977@gmail.com>
2020-04-27 02:45:11 +0200

    Dando estilos a los componentes de HTML5

--- src/components/ComponentsNativos.vue
+++ src/components/ComponentsNativos.vue
@@ -3,20 +3,151 @@
     <div class="row">
       <div class="col">
         <div id="resultID" v-text="id"></div>
-        <div name="resultNAME" v-text="name"></div>
+        <div name="resultATTR" v-text="attr"></div>
         <div class="resultCLASS" v-text="className"></div>
         <div v-text="tagName"></div>
-        <div>
+        <div class="container">
+          <table>
+            <tbody>
+              <tr>
+                <td>A1</td>
+                <td>B1</td>
+                <td>C1</td>
+              </tr>
+              <tr>
+                <td>A2</td>
+                <td>B2</td>
+                <td>C2</td>
+              </tr>
+              <tr>
+                <td>A3</td>
+                <td>B3</td>
+                <td>C3</td>
+              </tr>
+              <tr>
+                <td>A4</td>
+                <td>B4</td>
+                <td>C4</td>
+              </tr>
+              <tr>
+                <td>A5</td>
+                <td>B5</td>
+                <td>C5</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="container">
           <button id="btnID" @click="btnID">Click ID</button>
-          <button id="btnNAME" @click="btnNAME">Click NAME</button>
+          <button id="btnATTR" @click="btnATTR">Click ATTR</button>
           <button id="btnCLASS" @click="btnCLASS">Click CLASS</button>
           <button id="btnTAGNAME" @click="btnTAGNAME">Click TAGNAME</button>
         </div>
-        <div>
+        <div class="container">
           <button id="btnResetID" @click="btnResetID">Reset ID</button>
-          <button id="btnResetNAME" @click="btnResetNAME">Reset NAME</button>
+          <button id="btnResetATTR" @click="btnResetATTR">Reset ATTR</button>
           <button id="btnResetCLASS" @click="btnResetCLASS">Reset CLASS</button>
           <button id="btnResetTAGNAME" @click="btnResetTAGNAME">Reset TAGNAME</button>
+        </div>
+        <div class="container">
+          <form>
+            <div class="container">
+              <div class="input-float">
+                <input type="text" id="nombre" placeholder=" " />
+                <label for="nombre">Nombre</label>
+              </div>
+            </div>
+            <div class="container">
+              <div class="input-float">
+                <input type="text" id="apellido" placeholder=" " />
+                <label for="apellido">Apellido</label>
+              </div>
+            </div>
+            <div class="container">
+              <div class="input-float">
+                <input type="number" id="edad" placeholder=" " />
+                <label for="edad">Edad</label>
+              </div>
+            </div>
+            <div class="container">
+              <div class="input-float">
+                <input type="password" id="password" placeholder=" " />
+                <label for="password">Contraseña</label>
+              </div>
+            </div>
+            <div class="container">
+              <input type="radio" id="hombre" value="hombre">
+              <label for="hombre">Hombre</label><br>
+              <input type="radio" id="mujer" value="Mujer">
+              <label for="female">Mujer</label><br>
+              <input type="radio" id="otro" value="Otro">
+              <label for="other">Otro</label>
+            </div>
+            <div class="container">
+              <input type="checkbox" id="ordenador" value="Ordenador">
+              <label for="ordenador">Tengo ordenador</label><br>
+              <input type="checkbox" id="tablet" value="tablet">
+              <label for="tablet">Tengo tablet</label><br>
+              <input type="checkbox" id="Movil" value="Movil">
+              <label for="Movil">Tengo Movil</label><br>
+              <input type="checkbox" id="Portatil" value="Portatil">
+              <label for="Portatil">Tengo Portatil</label><br>
+            </div>
+            <div class="container">
+              <label for="color">Seleciona un color</label>
+              <input type="color">
+            </div>
+            <div class="container">
+              <label for="cumpleaños">Cumpleaños</label>
+              <input type="date" />
+            </div>
+            <div class="container">
+              <label for="cumpleaños">Hora de entrada</label>
+              <input type="datetime-local" />
+            </div>
+            <div class="container">
+              <label for="email">Email</label>
+              <input type="email" />
+            </div>
+            <div class="container">
+              <input type="file">
+            </div>
+            <div class="container">
+              <input type="image">
+            </div>
+            <div class="container">
+              <input type="month">
+            </div>
+            <div class="container">
+              <input type="range">
+            </div>
+            <div class="container">
+              <input type="search">
+            </div>
+            <div class="container">
+              <input type="tel">
+            </div>
+            <div class="container">
+              <label for="texto">Texto</label>
+              <input type="text">
+            </div>
+            <div class="container">
+              <label for="hora">Hora</label>
+              <input type="time">
+            </div>
+            <div class="container">
+              <label for="url">URL</label>
+              <input type="url">
+            </div>
+            <div class="container">
+              <label for="semana">Semana</label>
+              <input type="week">
+            </div>
+            <div class="container">
+              <input type="submit" value="Enviar" />
+              <input type="reset" value="Reset" />
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -32,7 +163,7 @@ export default {
   data() {
     return {
       id: '',
-      name: '',
+      attr: '',
       className: '',
       tagName: '',
     };
@@ -41,20 +172,20 @@ export default {
     btnID() {
       this.id = 'Acceso por el ID';
     },
-    btnNAME() {
-      this.name = 'Acceso por el NAME';
+    btnATTR() {
+      this.attr = 'Acceso por el Atributo';
     },
     btnCLASS() {
-      this.className = 'Acceso por la CLASE';
+      this.className = 'Acceso por la Clase';
     },
     btnTAGNAME() {
-      this.tagName = 'Acceso por el TAGNAME';
+      this.tagName = 'Acceso por el Tagname';
     },
     btnResetID() {
       this.id = '';
     },
-    btnResetNAME() {
-      this.name = '';
+    btnResetATTR() {
+      this.attr = '';
     },
     btnResetCLASS() {
       this.className = '';
@@ -68,7 +199,7 @@ export default {
 <style lang="scss" scoped>
 .row {
   .col {
-    div {
+    .container {
       min-height: 100px;
       box-shadow: 0 0 8px 2px rgba(0, 0, 0, .2);
       margin: 10px;
@@ -78,5 +209,57 @@ export default {
 }
 button {
   cursor: pointer;
+}
+table {
+  width: 100%;
+  border-collapse: collapse;
+  text-align: center;
+  border: 1px solid #dddddd;
+  tr {
+    td {
+      border: 1px solid #dddddd;
+      padding: 5px;
+    }
+  }
+}
+.input-float {
+  input {
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    border-bottom: 1px solid #cccccc;
+    width: 100%;
+    position: relative;
+    left: 0;
+    top: 15px;
+    background: transparent;
+    padding: 20px 10px 0 10px;
+    transition: 180ms ease-in-out;
+    &:not(:placeholder-shown) + label {
+      transition: 180ms ease-in-out;
+      top: -30px;
+      font-size: 10px;
+    }
+    &:focus + label {
+      transition: 180ms ease-in-out;
+      top: -30px;
+      font-size: 10px;
+    }
+    &:focus {
+      background: #ececec;
+      transition: 180ms ease-in-out;
+    }
+  }
+  label {
+    position: relative;
+    top: -7px;
+    left: 10px;
+    font-size: 14px;
+    color: #bbbbbb;
+    transition: 180ms ease-in-out;
+  }
+}
+:focus {
+  outline: 0px;
 }
 </style>
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,7 +2,52 @@ describe('Comprobamos que los botones de acciones funcionan', () => {
   it('Abrimos la aplicación para hacer el test', () => {
     cy.visit('/');
   });
-  it('Accedemos al elemento mediante el ID', () => {
-    cy.get('#btnID');
+  // Acceso por ID
+  it('Comprobamos que el resultado este vacio en este momento mediante el ID', () => {
+    cy.get('#resultID').should('contain', '');
+  });
+  it('Comprobamos que el resultado este relleno en este momento mediante el ID', () => {
+    cy.get('#btnID').click();
+    cy.get('#resultID').should('contain', 'Acceso por el ID');
+  });
+  // Acceso por Atributo
+  it('Comprobamos que el resultado este vacio en este momento mediante el Atributo', () => {
+    cy.get('[name=resultATTR]').should('contain', '');
+  });
+  it('Comprobamos que el resultado este relleno en este momento mediante el Atributo', () => {
+    cy.get('#btnATTR').click();
+    cy.get('[name=resultATTR]').should('contain', 'Acceso por el Atributo');
+  });
+  // Acceso por Clase
+  it('Comprobamos que el resultado este vacio en este momento mediante la Clase', () => {
+    cy.get('.resultCLASS').should('contain', '');
+  });
+  it('Comprobamos que el resultado este relleno en este momento mediante la Clase', () => {
+    cy.get('#btnCLASS').click();
+    cy.get('.resultCLASS').should('contain', 'Acceso por la Clase');
+  });
+  // Acceso por Tagname
+  it('Comprobamos que el resultado este vacio en este momento mediante el nombre de la etiqueta', () => {
+    cy.get('.col > :nth-child(4)').should('contain', '');
+  });
+  it('Comprobamos que el resultado este relleno en este momento mediante el nombre de la etiqueta', () => {
+    cy.get('#btnTAGNAME').click();
+    cy.get('.col > :nth-child(4)').should('contain', 'Acceso por el Tagname');
+  });
+  // Reset
+  it('Reseteamos todos los valores para dejarlos con el valor de origen', () => {
+    cy.get('#btnResetID').click();
+    cy.get('#resultID').should('contain', '');
+    cy.get('#btnResetATTR').click();
+    cy.get('[name=resultATTR]').should('contain', '');
+    cy.get('#btnResetCLASS').click();
+    cy.get('.resultCLASS').should('contain', '');
+    cy.get('#btnResetTAGNAME').click();
+    cy.get('.col > :nth-child(4)').should('contain', '');
+  });
+
+  // Test en la tabla
+  it('Comprobamos el nùmero de filas (registros)', () => {
+    cy.get('table tbody tr').should('have.length', 5);
   });
 });
